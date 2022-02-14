9facd292fdaf062a47c0a965f02577a0857edbe6
David Dao <contact.daviddao@gmail.com>
2022-02-14 05:37:15 +0100

    Update rumble.rb

--- rumble.rb
+++ rumble.rb
@@ -1,5 +1,5 @@
 class Rumble < Formula
-  desc "RumbleDB 1.17.0 'Cacao tree' for Apache Spark | Run queries on your large-scale, messy JSON-like data"
+  desc "RumbleDB 1.17.0 'Cacao tree' for Apache Spark"
   homepage "https://rumbledb.org/"
   url "https://github.com/daviddao/homebrew-rumble/releases/download/1.17.0-brew/rumble-brew.zip"
   sha256 "21ebe235b3ff0e306eea3ce2d28f5186da0edf5c64236f5feb43e12309538d3d"
