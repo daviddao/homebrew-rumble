91d4b5d255207c6f4eca624acb65acabe07cac88
David Dao <contact.daviddao@gmail.com>
2022-02-14 05:34:09 +0100

    Update rumble.rb

--- rumble.rb
+++ rumble.rb
@@ -1,9 +1,6 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-
 class Rumble < Formula
-  desc "RumbleDB 1.17.0 'Cacao tree' for Apache Spark | Run queries on your large-scale, messy JSON-like data (JSON, text, CSV, Parquet, ROOT, AVRO, SVM...) | No install required (just a jar to download) | Declarative Machine Learning and more"
-  homepage "http://rumbledb.org/"
+  desc "RumbleDB 1.17.0 'Cacao tree' for Apache Spark | Run queries on your large-scale, messy JSON-like data"
+  homepage "https://rumbledb.org/"
   url "https://github.com/daviddao/homebrew-rumble/releases/download/1.17.0-brew/rumble-brew.zip"
   sha256 "21ebe235b3ff0e306eea3ce2d28f5186da0edf5c64236f5feb43e12309538d3d"
   license "MIT"
